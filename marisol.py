3a75e561451fc7ee2179546d25f25443c8731248
Marisol <marisol.quintana.acosta@gmail.com>
2016-05-14 15:30:31 -0600

    Mi app Django Girls, primer commit

---
+++ marisol.py
@@ -0,0 +1,31 @@
+name = "sonja"
+
+
+def marisol(name):
+	if name =="ola":
+		print ("hey hola!")
+	elif name == "sonja":
+		print ("hey aloha!")
+	elif name == "sonjita":
+		print ("sonjita")
+	else:
+		print ("sonjuuu")
+
+
+print ("marisolita")
+marisol (name)
+marisol ("hola")
+marisol ("sonja")
+marisol ("sonjita")
+
+nombres= ["juan","paco","pedro","delamar"]
+
+def quintana(new):
+	print (new)
+
+for nombre in nombres:
+	quintana (nombres)
+maxima = 5
+
+for actual in range (1,maxima):
+	print (actual)
